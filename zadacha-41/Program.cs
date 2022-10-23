2619a7f8454faa42dba5fd319c5a0ca89fa94cbb
Galtseva Ekaterina <katerina.vedernikova-95@mail.ru>
2022-10-23 20:09:07 +0500

    Решение шестой домашней работы

---
+++ zadacha-41/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Введите элементы через пробел :");
+
+int[] array = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+int count = array.Count(X => X >0);
+Console.Write($"Чисел больше 0 -> {count}");
---
+++ zadacha-43/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Write("Введите значение b1: ");
+string B1 = Console.ReadLine();
+double b1 = double.Parse(B1);
+
+Console.Write("Введите значение k1: ");
+string K1 = Console.ReadLine();
+double k1 = double.Parse(K1);
+
+Console.Write("Введите значение b2: ");
+string B2 = Console.ReadLine();
+double b2 = double.Parse(B2);
+
+Console.Write("Введите значение k2: ");
+string K2 = Console.ReadLine();
+double k2 = double.Parse(K2);
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"Координаты точки пересечения: ({x}, {y})");
